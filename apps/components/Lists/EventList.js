9faf51ee6d41a552ac7612c803432e9e2ab00c6b
takaya787 <takaya318318@gmail.com>
2021-02-15 18:49:14 +0900

    Event情報をlistに表示

--- apps/components/Lists/EventList.js
+++ apps/components/Lists/EventList.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect, useContext } from 'react'
-
 import styles from './EventList.module.css'
+//others
+import { EventsContext } from '../../pages/_app'
+
+
 //props {activeDate: date}
 export const EventList = (props) => {
-  const [actualEvents, setactualEvents] = useState([])
+  const { events } = useContext(EventsContext);
+  const [actualEvents, setActualEvents] = useState([])
+  //表示するeventsを決定
+  useEffect(function () {
+    setActualEvents(events)
+  }, [events])
 
   return (
     <div className={styles.eventlist}>
       <h3 className={styles.title}>Event一覧</h3>
       <ul className={styles.lists}>
-        <li className={styles.list_each}>sample 1</li>
-        <li className={styles.list_each}>sample 2</li>
-        <li className={styles.list_each}>sample 3</li>
-        <li className={styles.list_each}>sample 1</li>
-        <li className={styles.list_each}>sample 2</li>
-        <li className={styles.list_each}>sample 3</li>
-        <li className={styles.list_each}>sample 1</li>
-        <li className={styles.list_each}>sample 2</li>
-        <li className={styles.list_each}>sample 3</li>
+        {actualEvents.map(event => (
+          <li key={event.id} className={styles.list_each}>{event.title}</li>
+        ))}
       </ul>
     </div>
   )

daf05a2f5b3dd906c80a86aedb252c113feba466
takaya787 <takaya318318@gmail.com>
2021-02-15 11:27:58 +0900

    logoutのデモ機能　完成

--- apps/pages/index.js
+++ apps/pages/index.js
@@ -1,19 +1,36 @@
+import React, { useContext } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 //components
 import { Main } from '../components/Main'
 import { Modal } from '../components/Modal'
-// import { UserForm } from '../components/Forms/UserForm'
-// import { LoginForm } from '../components/Forms/LoginForm'
+//others
+import Auth from '../modules/auth'
+import { UserContext } from './_app'
 
 export default function Home() {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    setUser({ email: '', id: 0, name: '' });
+    Auth.logout();
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Calender App</title>
       </Head>
       <h1>カレンダーでスケジュール管理！</h1>
-      <Modal />
+      {!Auth.isLoggedIn() && (
+        <Modal />
+      )}
+      {Auth.isLoggedIn() && (
+        <>
+          <p>Welcome {user.name}!</p>
+          <button onClick={handleLogout}>Log out</button>
+        </>
+      )}
       <Main />
     </div>
   )

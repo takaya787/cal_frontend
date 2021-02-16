bf899667a61b72ea3ae850ff901f8388a3e9edb5
takaya787 <takaya318318@gmail.com>
2021-02-16 11:25:13 +0900

    eventlistの表示設定　完了

--- apps/components/Lists/EventList.js
+++ apps/components/Lists/EventList.js
@@ -6,20 +6,27 @@ import { EventsContext } from '../../pages/_app'
 
 //props {activeDate: date}
 export const EventList = (props) => {
+  const active_year = props.activeDate.getFullYear()
+  const active_month = props.activeDate.getMonth() + 1
+  const active_date = props.activeDate.getDate()
+
   const { events } = useContext(EventsContext);
   const [actualEvents, setActualEvents] = useState([])
   //表示するeventsを決定
   useEffect(function () {
-    setActualEvents(events)
-  }, [events])
+    const filteredevents = events.filter((event) => {
+      return (event.month === active_month && event.year === active_year)
+    })
+    setActualEvents(filteredevents)
+  }, [events, props.activeDate])
 
   return (
     <>
-      <h3 className={styles.title}>予定一覧　{props.activeDate.getFullYear()}年 {props.activeDate.getMonth() + 1}月 </h3>
+      <h3 className={styles.title}>予定一覧　{active_year}年 {active_month}月 </h3>
       <div className={styles.eventlist}>
         <ul className={styles.lists}>
           {actualEvents.map(event => (
-            <li key={event.id} className={styles.list_each}>{event.title}</li>
+            <li key={event.id} className={styles.list_each}><span className={styles.list_date}>{event.date}日</span>{event.title} </li>
           ))}
         </ul>
       </div>

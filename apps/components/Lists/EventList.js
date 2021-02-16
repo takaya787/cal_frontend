88d25b4c96aa35facb7cf1358debae5cbaf7b64d
takaya787 <takaya318318@gmail.com>
2021-02-16 11:49:02 +0900

    過去の予定は色付け

--- apps/components/Lists/EventList.js
+++ apps/components/Lists/EventList.js
@@ -8,7 +8,23 @@ import { EventsContext } from '../../pages/_app'
 export const EventList = (props) => {
   const active_year = props.activeDate.getFullYear()
   const active_month = props.activeDate.getMonth() + 1
-  const active_date = props.activeDate.getDate()
+  // const active_date = props.activeDate.getDate()
+  //日付を確認用のコード
+  const today = new Date()
+  // const today_date = today.getDate()
+
+  //eventの日付と今日の日付を比べて、過去かどうかを調べる
+  const list_class = (today, target_date, target_month, target_year) => {
+    const today_date = today.getDate()
+    const today_month = today.getMonth() + 1
+    const today_year = today.getFullYear()
+
+    if (today_date > target_date || today_month > target_month || today_year > target_year) {
+      return `${styles.list_each} ${styles.past}`
+    } else {
+      return styles.list_each
+    }
+  }
 
   const { events } = useContext(EventsContext);
   const [actualEvents, setActualEvents] = useState([])
@@ -26,7 +42,7 @@ export const EventList = (props) => {
       <div className={styles.eventlist}>
         <ul className={styles.lists}>
           {actualEvents.map(event => (
-            <li key={event.id} className={styles.list_each}><span className={styles.list_date}>{event.date}日</span>{event.title} </li>
+            <li key={event.id} className={list_class(today, event.date, event.month, event.year)}><span className={styles.list_date}>{event.date}日</span>{event.title} </li>
           ))}
         </ul>
       </div>

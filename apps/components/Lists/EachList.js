ca356a0cab652257e6406b90f742d88817838c6f
takaya787 <takaya318318@gmail.com>
2021-02-19 10:47:50 +0900

    表示ボタンの色分け完了

--- apps/components/Lists/EachList.js
+++ apps/components/Lists/EachList.js
@@ -5,18 +5,18 @@ import styles from './EventList.module.css'
 export const EachList = (props) => {
   //memo情報を表示するか決定
   const [showMemo, setShowMemo] = useState(false);
+  //memo partの長さを調整するclass
   const each_style = () => {
-    // if (showMemo) {
-    //   return `${styles.list_each} ${styles.longer}`
-    // } else {
-    //   return `${styles.list_each}`
-    // }
     return showMemo ? `${styles.list_each} ${styles.longer}` : `${styles.list_each}`
+  }
+  //switchの色を変更する
+  const switch_style = () => {
+    return showMemo ? `${styles.list_switch} ${styles.blue}` : `${styles.list_switch}`
   }
 
   return (
     <div className={each_style()}>
-      <span className={styles.list_date}>{props.date}日</span>{showMemo ? <button className={styles.list_switch} onClick={() => setShowMemo(false)}>詳細閉じる</button> : <button className={styles.list_switch} onClick={() => setShowMemo(true)}>詳細確認</button>}
+      <span className={styles.list_date}>{props.date}日</span>{showMemo ? <button className={switch_style()} onClick={() => setShowMemo(false)}>詳細閉じる</button> : <button className={switch_style()} onClick={() => setShowMemo(true)}>詳細確認</button>}
       <br />{props.title}
       {/* 内容を表示するボタン */}
       <br />
